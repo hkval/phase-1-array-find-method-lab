d845f6083bae6eed9547263c3a3e5fd3b5aadf89
Holly <hollykvalheim@gmail.com>
2022-10-29 16:04:50 -0700

    commit tree ac3377d1f5a2ded018108fe166f59c148ba156c3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function myCallback(element, index, array){
+    return (element.result === 'W')
+}
+
+
+
+function superbowlWin(array){
+    const elementFound = array.find(myCallback)
+    return !!elementFound ? elementFound.year : undefined
+
+}
